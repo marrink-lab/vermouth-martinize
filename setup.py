8bbf5fc16a6e03eaf32bd0b92202d23b906ec8e0
Peter <pckroon@users.noreply.github.com>
2017-10-05 13:09:05 +0200

    Made a functioning setup cfg/py combo

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
-# Do somethings...
+import os
 
-#intall_requirements = [numpy, networkx ~= 2.0]
+from setuptools import setup
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+setup(package_data={'': package_files('martinize2/mapping')})
+
