be51206fc17ce1dce8eadbbe267a433eb9795a97
Jonathan Barnoud <jonathan@barnoud.net>
2017-10-24 16:25:59 +0200

    Improve styles and comments of the ITP writer
    Also change the name from write_itp to write_molecule_itp as we will
    likely have a function that will write an itp file for a full system or
    for a force field latter on.

--- martinize2/gmx/itp.py
+++ martinize2/gmx/itp.py
@@ -1,40 +1,82 @@
+"""
+Handle the ITP file format from Gromacs.
+"""
+
+from __future__ import print_function
 import copy
 
-def write_itp(molecule, outfile):
-    try:
-        moltype = molecule.moltype
-    except AttributeError:
-        moltype = None
-    if moltype is None:
-        raise ValueError('Molecule has no moltype.')
+__all__ = ['write_molecule_itp', ]
 
+def _attr_has_not_none_attr(obj, attr):
+    """
+    Raise a Value error is 'obj' does not have an attribute 'attr' or if its
+    value is ``None``.
+    """
     try:
-        nrexcl = molecule.nrexcl
+        value = getattr(obj, attr)
     except AttributeError:
-        nrexcl = None
-    if nrexcl is None:
-        raise ValueError('Molecule has no nrexcl.')
+        value = None
+    if value is None:
+        raise ValueError('{} has no attribute "{}".'.format(obj, attr))
 
-    max_length = {}
+
+def write_molecule_itp(molecule, outfile):
+    """
+    Write a molecule in ITP format.
+
+    The molecule must have a `moltype` and a `nrexcl` attribute. Each atom
+    in the molecule must have at least the following keys: `atype`, `resid`,
+    `resname`, `atomname`, and `charge_group`. Atoms can also have a
+    `charge` and a `mass` key.
+
+    Parameters
+    ----------
+    molecule: Molecule
+        The molecule to write. See above for the minimal information the
+        molecule must contain.
+    outfile: file handler or file-like
+        The file in which to write.
+
+    Raises
+    ------
+    ValueError
+        The molecule is missing required information.
+    """
+    # Make sure the molecule contains the information required to write the
+    # header.
+    _attr_has_not_none_attr(molecule, 'moltype')
+    _attr_has_not_none_attr(molecule, 'nrexcl')
+
+    # Make sure the molecule contains the information required to write the
+    # [atoms] section. The charge and mass can be ommited, if so gromacs take
+    # them from the [atomtypes] section of the ITP file.
     for attribute in ('atype', 'resid', 'resname', 'atomname',
                       'charge_group'):
         if not all([attribute in atom for _, atom in molecule.atoms]):
             raise ValueError('Not all atom have a {}.'.format(attribute))
+
+    # Get the maximum length of each atom field so we can align the fields.
+    # Atom indexes are written as a consecutive series starting from 1.
+    # The maximum index of a 0-based series is `len(x) - 1`; because the
+    # series starts at 1, the maximum value is `len(x).
+    max_length = {'idx': len(str(len(molecule)))}
     for attribute in ('atype', 'resid', 'resname', 'atomname',
                       'charge_group', 'charge', 'mass'):
         max_length[attribute] = max(len(str(atom.get(attribute, '')))
                                     for _, atom in molecule.atoms)
-    # Atom indexes are written as a continuous series starting from 1.
-    # The maximum index of a 0-based series is `len(x) - 1`, because the
-    # series starts at 1, the maximum value is `len(x).
-    max_length['idx'] = len(str(len(molecule)))
-
-
 
     print('[ moleculetype ]', file=outfile)
-    print(moltype, nrexcl, file=outfile)
+    print(molecule.moltype, molecule.nrexcl, file=outfile)
     print('', file=outfile)
 
+    # The atoms in the [atoms] section must be consecutively numbered, yet
+    # there is no guarantee that the molecule fulfill that constrain.
+    # Therefore we renumber the atoms. The `correspondence` dict allows to
+    # keep track of the correspondence between the original and the new
+    # numbering so we can apply the renumbering to the interactions.
+    # The resid and charge_group should also be consecutive, though this is
+    # left as the user responsibility. Make sure residues and charge groups are
+    # correctly numbered.
     correspondence = {}
     print('[ atoms ]', file=outfile)
     for idx, (original_idx, atom) in enumerate(molecule.atoms, start=1):
