39a53b96866c9449e7f8b79d44f6c21960049e79
Peter <pckroon@users.noreply.github.com>
2017-11-06 11:22:09 +0100

    Did some cleaning, and added some references to wikipedia

--- embed_test.py
+++ embed_test.py
@@ -41,10 +41,11 @@ def make_laplacian(adjacency_matrix):
 
 
 def spectral(G, ndim=3):
+    # https://networkx.github.io/documentation/networkx-2.0/reference/generated/networkx.drawing.layout.spectral_layout.html
 #    conmat = nx.adjacency_matrix(G, weight='weight')
 #    L = make_laplacian(conmat.A)
 #    print(L.A)
-    L = nx.normalized_laplacian_matrix(G)
+    L = nx.normalized_laplacian_matrix(G, weight='weight')
     vals, vecs = ssl.eigsh(L, k=ndim+1, sigma=-1e-7, which='LM')
 #    S = np.linalg.eig(L.A)
 #    order = np.argsort(S[0])
@@ -55,10 +56,10 @@ def spectral(G, ndim=3):
     return vecs[:, 1:ndim+1] * scale
 
 def MDS(G, fill=10, ndim=3):
+    # https://en.wikipedia.org/wiki/Multidimensional_scaling
     n = len(G)
-    distmat = nx.adjacency_matrix(G).A
+    distmat = nx.adjacency_matrix(G, weight='distance').A
     distmat[distmat == 0] = fill
-#    distmat = ssd.squareform(ssd.pdist([G.node[idx]['position'] for idx in G.node]))
     D2 = distmat ** 2
     J = np.eye(n) - 1/n * np.ones(n) * np.ones(n).T
     # Must be matmul
@@ -68,12 +69,13 @@ def MDS(G, fill=10, ndim=3):
     vals = S[0][order]
     vecs = S[1][:, order]
     M = np.real(vecs[:, :ndim])
-    L = np.sqrt(np.abs(vals[:ndim]))
-    return M * L
+    L = np.diag(np.sqrt(vals[:ndim]))
+    return M @ L
 
-def smacof_dense(G, eps=1e-2, maxiter=5000, ndim=3, start=None):
+def smacof_dense(G, eps=1e-1, maxiter=5000, ndim=3, start=None):
+    # https://en.wikipedia.org/wiki/Stress_majorization
     n = len(G)
-    if start is not None:
+    if start is None:
         X = np.random.rand(n, ndim)
     else:
         X = start
@@ -81,44 +83,52 @@ def smacof_dense(G, eps=1e-2, maxiter=5000, ndim=3, start=None):
     w = np.zeros_like(disparities)
     w[disparities != 0] = 1
     w *= (n*(n-1))/(w * disparities**2).sum()
-    V = -w
-    V[np.arange(len(V)), np.arange(len(V))] = np.sum(w, axis=1)
+    V = np.diag(np.sum(w, axis=1))
+    V -= w
     V = np.linalg.pinv(V)
     for it in range(maxiter):
         dis = ssd.squareform(ssd.pdist(X))
+        stress = np.sum(w * (dis - disparities)**2)
+        if stress < eps:
+            print('Converged after {} iterations'.format(it))
+            break
         dis[dis == 0] = np.inf
         # Update X using the Guttman transform
         S = 1/dis
-        B = -w * disparities * S
-        B[np.arange(len(B)), np.arange(len(B))] = np.sum(w * disparities * S, axis=1)
+        B = np.diag(np.sum(w * disparities * S, axis=1))
+        B -= w * disparities * S
         displacement = V @ B
         X = displacement @ X
-        # Maybe this should depend on the number of atoms or something
-        if np.max(np.abs(np.diag(displacement) - 1)) < eps:
-            print('Converged after {} iterations'.format(it))
-            break
 
     return X
 
 
-def smacof(G, eps=1e-2, maxiter=5000, ndim=3, start=None):
+def smacof(G, eps=1e-1, maxiter=5000, ndim=3, start=None):
+    # https://en.wikipedia.org/wiki/Stress_majorization
     n = len(G)
-    if start is not None:
+    if start is None:
         X = np.random.rand(n, ndim)
     else:
         X = start
     disparities = nx.adjacency_matrix(G, weight='distance')
     data = [n*(n-1)/disparities.power(2).sum()] * disparities.nnz
     w = ss.csc_matrix((data, disparities.nonzero()), shape=disparities.shape)
-    
-    # The pseudo-inverse will be dense anyway.
-    V = -w.A
-    V[np.diag_indices_from(V)] = np.array(w.sum(axis=0))[0]
-    V = np.linalg.pinv(V)
+
+    # The pseudo-inverse will be dense anyway. And let's make sure it's an array.
+    V = np.diag(w.sum(axis=0).A[0])
+    V -= w
+    V = np.linalg.pinv(V).A
     for it in range(maxiter):
         vecs = (X[w.nonzero()[0]] - X[w.nonzero()[1]])
-        data = 1/np.sqrt(np.sum(vecs**2, axis=-1))
-        S = ss.csr_matrix((data, w.nonzero()), w.shape)
+        # vecs is dense. I don't think this can be avoided.
+        # dis however, is sparse.
+        dis = np.sqrt(np.sum(vecs**2, axis=-1))
+        dis = ss.csr_matrix((dis, w.nonzero()), w.shape)
+        stress = (w * (dis - disparities).power(2)).sum()
+        if stress < eps:
+            print('Converged after {} iterations'.format(it))
+            break
+        S = dis.power(-1)
         # Update X using the Guttman transform
         B = -w.multiply(disparities).multiply(S)
         diag = np.array(-B.sum(axis=0))[0]
@@ -126,10 +136,6 @@ def smacof(G, eps=1e-2, maxiter=5000, ndim=3, start=None):
         # V is dense, so displacements is dense.
         displacement = V @ B
         X = displacement @ X
-        # Maybe this should depend on the number of atoms or something
-        if np.max(np.abs(np.diag(displacement) - 1)) < eps:
-            print('Converged after {} iterations'.format(it))
-            break
 
     return X
 
@@ -142,8 +148,7 @@ def dist_to_weight(distance):
 #    ax.scatter(mat[:, 0], mat[:, 1], mat[:, 2])
 
 for idx, jdx in mol.edges():
-    w = mol[idx][jdx].get('weight', 1)
-    d = 1/w
+    d = mol[idx][jdx].get('distance', 1)
     mol[idx][jdx]['distance'] = d
     mol[idx][jdx]['weight'] = dist_to_weight(d)
 
@@ -163,10 +168,11 @@ for idx, jdx, kdx in paths:
     mol_angle.add_edge(idx, kdx, distance=length, weight=dist_to_weight(length))
 print('Added {} angles.'.format(len(paths)))
 
-#embedded = MDS(mol_angle)
+#embedded = MDS(mol_angle, fill=1000)
 embedded = spectral(mol_angle)
-embedded = smacof_dense(mol_angle, eps=2e-2, maxiter=5000, start=embedded)
-#embedded = smacof(mol_angle, maxiter=5000)
+embedded = smacof_dense(mol_angle, maxiter=5000, start=embedded)
+#embedded = smacof_dense(mol_angle, eps=2e-2, maxiter=5000)
+#embedded = smacof(mol_angle, maxiter=5000, start=embedded)
 
 orig_pos = np.array([mol.node[idx]['position'] for idx in mol.node])
 
