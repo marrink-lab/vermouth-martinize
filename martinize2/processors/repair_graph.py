3d933813c043dfa6cbb1ced2e51c0413eefbfa68
Peter <pckroon@users.noreply.github.com>
2017-11-06 12:59:54 +0100

    Undo modification of MCS algorithm. Git is hard.

--- martinize2/processors/repair_graph.py
+++ martinize2/processors/repair_graph.py
@@ -11,8 +11,8 @@ from .processor import Processor
 from ..utils import first_alpha, maxes
 from ..graph_utils import *
 
-from collections import Counter
 import functools
+import itertools
 import os.path
 
 import networkx as nx
@@ -42,6 +42,12 @@ def read_reference_graph(resname):
     """
     return nx.read_gml(os.path.join(DATA_PATH, 'universal', '{}.gml'.format(resname)), label='id')
 #    return nx.read_gml('/universal/{}.gml'.format(resname), label='id')
+
+
+def add_element_attr(molecule):
+    for node_idx in molecule:
+        node = molecule.node[node_idx]
+        node['element'] = node.get('element', first_alpha(node['atomname']))
 
 
 def make_reference(mol):
@@ -88,15 +94,13 @@ def make_reference(mol):
         # TODO: make separate function for just one residue.
         # TODO: multiprocess this loop?
         # TODO: Merge degree 1 nodes (hydrogens!) with the parent node. And
-        #       check whether the node degrees match? I don't think this is
-        #       possible, since there are atoms missing.
-        # TODO: missing residues? Should that be done here?
+        # check whether the node degrees match?
 
-        resname = residues.nodes[residx]['resname']
-        resid = residues.nodes[residx]['resid']
-        chain = residues.nodes[residx]['chain']
+        resname = residues.node[residx]['resname']
+        resid = residues.node[residx]['resid']
+        chain = residues.node[residx]['chain']
         # print("{}{}".format(resname, resid), flush=True)
-        residue = residues.nodes[residx]['graph']
+        residue = residues.node[residx]['graph']
         reference = read_reference_graph(resname)
         add_element_attr(reference)
         add_element_attr(residue)
@@ -114,9 +118,9 @@ def make_reference(mol):
             # supposedly hard. This means we can't do the subgraph isomorphism like
             # we're used to. Instead, identify the atoms in the largest common
             # subgraph, and do the subgraph isomorphism/alignment on those. MCS is
-            # *ridiculously* expensive, so we only do it when we have to.
+            # ridiculously expensive, so we only do it when we have to.
             try:
-                mcs_match = max(maximum_common_subgraph(reference, residue, ['element']),
+                mcs_match = max(categorical_maximum_common_subgraph(reference, residue, ['element']),
                                 key=lambda m: rate_match(reference, residue, m))
             except ValueError:
                 raise ValueError('No common subgraph found between {} and reference {}'.format(resname, resname))
@@ -127,87 +131,12 @@ def make_reference(mol):
             res = residue.subgraph(mcs_match.values())
             matches = isomorphism(reference, res)
         match = matches[0]
-        # found=residue.copy(), because repair_residue needs to add missing
-        # atoms to it.
-        reference_graph.add_node(residx, chain=chain, reference=reference,
-                                 found=residue.copy(), resname=resname,
-                                 resid=resid, match=match)
-
+        reference_graph.add_node(residx, chain=chain, reference=reference, found=residue, resname=resname, resid=resid, match=match)
     reference_graph.add_edges_from(residues.edges())
     return reference_graph
 
 
-def repair_residue(molecule, ref_residue):
-    # if ref_residue['found'].graph.get('canonized', False):
-    #    return
-    # Rebuild missing atoms and canonicalize atomnames
-    missing = []
-    # Step 1: find all missing atoms. Canonicalize names while we're at it.
-    reference = ref_residue['reference']
-    found = ref_residue['found']
-    # found.graph['canonized'] = True
-    match = ref_residue['match']
-
-    resid = ref_residue['resid']
-    resname = ref_residue['resname']
-
-    for ref_idx in reference:
-        if ref_idx in match:
-            res_idx = match[ref_idx]
-            node = molecule.nodes[res_idx]
-            # Copy, because it's references everywhere.
-            node['graph'] = molecule.subgraph([res_idx]).copy()
-            node.update(reference.nodes[ref_idx])
-        else:
-#            if reference.nodes[ref_idx]['element'] != 'H':
-            print('Missing atom {}{}:{}'.format(resname, resid, reference.nodes[ref_idx]['atomname']))
-            missing.append(ref_idx)
-    # Step 2: try to add all missing atoms one by one. As long as we added
-    # *something* the situation changed, and we might be able to place another.
-    # We can only place atoms for which we know a neighbour.
-    added = True
-    while missing and added:
-        added = False
-        for ref_idx in missing:
-            # See if the atom we want to add has a neighbour for which we know
-            # the position. Otherwise, continue to the next.
-            if all(ref_neighbour in missing for ref_neighbour in reference[ref_idx]):
-                continue
-            added = True
-            missing.pop(missing.index(ref_idx))
-            res_idx = max(molecule) + 1  # Alternative: find the first unused number
-
-            # Create the new node
-            match[ref_idx] = res_idx
-            molecule.add_node(res_idx)
-            node = molecule.nodes[res_idx]
-            for key, val in ref_residue.items():
-                # Some attributes are only relevant on a residue level, not on
-                # an atom level.
-                if key not in ('match', 'found', 'reference'):
-                    node[key] = val
-            node.update(reference.nodes[ref_idx])
-
-            found.add_node(res_idx, **node)
-            # print("Adding {}{}:{}".format(resname, resid, node['atomname']))
-
-            neighbours = 0
-            for neighbour_ref_idx in reference[ref_idx]:
-                try:
-                    neighbour_res_idx = match[neighbour_ref_idx]
-                except KeyError:
-                    continue
-                if not molecule.has_edge(neighbour_res_idx, res_idx):
-                    molecule.add_edge(neighbour_res_idx, res_idx)
-                    neighbours += 1
-            assert neighbours != 0
-    if missing:
-        for ref_idx in missing:
-            print('Could not reconstruct atom {}{}:{}'.format(reference.nodes[ref_idx]['resname'],
-                  reference.nodes[ref_idx]['resid'], reference.nodes[ref_idx]['atomname']))
-
-
-def repair_graph(molecule, reference_graph):
+def repair_graph(aa_graph, reference_graph):
     """
     Repairs a graph ``aa_graph`` produced from a PDB file based on the
     information in ``reference_graph``. Missing atoms will be reconstructed and
@@ -241,42 +170,79 @@ def repair_graph(molecule, reference_graph):
         A new graph like ``aa_graph``, but with missing atoms (as per
         ``reference_graph``) added, and canonicalized atom and residue names.
     """
-    molecule = molecule.copy()
-    PTMs = []
+    mol = aa_graph.copy()
     for residx in reference_graph:
-        residue = reference_graph.nodes[residx]
-        repair_residue(molecule, residue)
-        # Atomnames are canonized, and missing atoms added
-        found = reference_graph.nodes[residx]['found']
-        match = reference_graph.nodes[residx]['match']
-        resid = reference_graph.nodes[residx]['resid']
-        
-        # Find the PTMs (or termini, or other additions) for *this* residue
-        extra = set(found.nodes) - set(match.values())
-        if extra:
-            # match: reference -> found
-            elem_counts = Counter(molecule.nodes[idx]['element'] for idx in extra)
-            attachments = set()
-            for idx in extra:
-                for n_idx in molecule.neighbors(idx):
-                    if n_idx not in extra:
-                        # Note that we are storing the indices in the molecule
-                        attachments.add(n_idx)
-            PTMs.append((residx, elem_counts, attachments))
-    # All residues have been canonicalized. Now we can go and find our PTMs.
-    # What we should do is find which PTM this is, get/make a new reference for
-    # the affected residues, and call repair_residue on them again.
-    # For now, just print them...
-    for resid, counts, n_idxs in PTMs:
-        print('Extra atoms for residue {}{}'.format(reference_graph.nodes[resid]['resname'], resid+1), end=':')
-        print(counts, end='; ')
-        for n_idx in n_idxs:
-            resname = molecule.nodes[n_idx]['resname']
-            resid = molecule.nodes[n_idx]['resid']
-            atomname = molecule.nodes[n_idx]['atomname']
-            print('Attached to: {}{}:{}'.format(resname, resid, atomname), end=', ')
-        print()
-    return molecule
+        # Rebuild missing atoms and canonicalize atomnames
+        missing = []
+        # Step 1: find all missing atoms. Canonicalize names while we're at it.
+        reference = reference_graph.node[residx]['reference']
+        match = reference_graph.node[residx]['match']
+        chain = reference_graph.node[residx]['chain']
+        resid = reference_graph.node[residx]['resid']
+        resname = reference_graph.node[residx]['resname']
+        for ref_idx in reference:
+            if ref_idx in match:
+                res_idx = match[ref_idx]
+                node = mol.node[res_idx]
+                node['graph'] = aa_graph.subgraph([res_idx])
+                node['atomname'] = reference.node[ref_idx]['atomname']
+                node['element'] = reference.node[ref_idx]['element']
+            else:
+#                if reference.node[ref_idx]['element'] != 'H':
+                print('Missing atom {}{}:{}'.format(resname, resid, reference.node[ref_idx]['atomname']))
+                missing.append(ref_idx)
+        # Step 2: try to add all missing atoms one by one. As long as we added
+        # *something* the situation changed, and we might be able to place another.
+        # We can only place atoms for which we know a neighbour.
+        added = True
+        while missing and added:
+            added = False
+            for ref_idx in missing:
+                # See if the atom we want to add has a neighbour for which we know
+                # the position. Otherwise, continue to the next.
+                if all(ref_neighbour in missing for ref_neighbour in reference[ref_idx]):
+                    continue
+                added = True
+                missing.pop(missing.index(ref_idx))
+                res_idx = max(mol) + 1  # Alternative: find the first unused number
+
+                # Create the new node
+                match[ref_idx] = res_idx
+                mol.add_node(res_idx)
+                node = mol.node[res_idx]
+                # TODO: Just copy all the attributes we have instead of listing
+                # them everywhere. Maybe. We don't need all attributes (match,
+                # found, reference).
+#                node['position'] = np.zeros(3)
+                node['chain'] = chain
+                node['resname'] = resname
+                node['resid'] = resid
+                node['atomname'] = reference.node[ref_idx]['atomname']
+                node['element'] = reference.node[ref_idx]['element']
+#                print("Adding {}{}:{}".format(resname, resid, node['atomname']))
+
+                neighbours = 0
+                for neighbour_ref_idx in reference[ref_idx]:
+                    try:
+                        neighbour_res_idx = match[neighbour_ref_idx]
+                    except KeyError:
+                        continue
+                    if not mol.has_edge(neighbour_res_idx, res_idx):
+                        mol.add_edge(neighbour_res_idx, res_idx)
+#                        node['position'] += mol.node[neighbour_res_idx]['position']
+                        neighbours += 1
+                assert neighbours != 0
+#                if neighbours == 1:
+#                    # Don't put atoms right on top of each other. Otherwise we'll
+#                    # see some segfaults from MD software.
+#                    node['position'] += np.random.normal(0, 0.01, size=3)
+#                else:
+#                    node['position'] /= neighbours
+        if missing:
+            for ref_idx in missing:
+                print('Could not reconstruct atom {}{}:{}'.format(reference.node[ref_idx]['resname'],
+                      reference.node[ref_idx]['resid'], reference.node[ref_idx]['atomname']))
+    return mol
 
 
 class RepairGraph(Processor):
