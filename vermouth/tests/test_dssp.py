c263bbb1546bfe953700ee030bb7b9238788cd37
Jonathan Barnoud <jonathan@barnoud.net>
2018-03-23 13:14:41 +0100

    Fix test_dssp
    This test suite what somehow forgotten when the lib got renamed.

--- vermouth/tests/test_dssp.py
+++ vermouth/tests/test_dssp.py
@@ -12,26 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.import pytest
 
+import pytest
 import itertools
 import os
-import martinize2 as m2
-import martinize2.dssp.dssp as dssp
-from martinize2.pdb.pdb import read_pdb
-from martinize2.tests.datafiles import (
+import vermouth
+import vermouth.dssp.dssp as dssp
+from vermouth.pdb.pdb import read_pdb
+from vermouth.tests.datafiles import (
     PDB_PROTEIN,
     PDB_NOT_PROTEIN,
     PDB_PARTIALLY_PROTEIN,
     DSSP_OUTPUT,
 )
 
-DSSP_EXECUTABLE = os.environ.get('MART2_TEST_DSSP', 'dssp')
+DSSP_EXECUTABLE = os.environ.get('VERMOUTH_TEST_DSSP', 'dssp')
 SECSTRUCT_1BTA = list('CEEEEETTTCCSHHHHHHHHHHHHTCCTTCCCSHHHHHHHHTTT'
                       'SCSSEEEEEESTTHHHHTTTSSHHHHHHHHHHHHHTTCCEEEEEC')
 
 
 @pytest.mark.parametrize(
     'molecule, reference_answer',
-    [(read_pdb(path), answer) for path, answer in [
+    [(read_pdb(str(path)), answer) for path, answer in [
         (PDB_PROTEIN, True),
         (PDB_NOT_PROTEIN, False),
         (PDB_PARTIALLY_PROTEIN, False),
@@ -41,43 +42,40 @@ def test_is_protein(molecule, reference_answer):
     """
     Make sure that proteins are correctly identified as such.
     """
-    assert dssp.is_protein(molecule) == reference_answer
+    assert vermouth.selectors.is_protein(molecule) == reference_answer
 
 
 @pytest.mark.parametrize(
     'atom, reference_answer',
     (
-        ({'position': [0, 0, 0]}, False),
-        ({'position': None}, True),
-        ({}, True),
+        ({'position': [0, 0, 0]}, True),
+        ({'position': None}, False),
+        ({}, False),
     )
 )
 def test_selector_no_position(atom, reference_answer):
-    assert dssp.selector_no_position(atom) == reference_answer
+    assert vermouth.selectors.selector_has_position(atom) == reference_answer
 
 
-def test_filter_out():
+def test_filter_minimal():
     # Build a molecule that has all even atoms with no positions.
-    molecule = read_pdb(PDB_PROTEIN)
+    molecule = read_pdb(str(PDB_PROTEIN))
     for atom in list(molecule.nodes.values())[::2]:
         atom['position'] = None
     # This means that we want to keep the odd atoms
     to_keep = list(molecule.nodes)[1::2]
 
-    filtered = dssp.filter_out(molecule, selector=dssp.selector_no_position)
+    filtered = vermouth.selectors.filter_minimal(
+        molecule,
+        selector=vermouth.selectors.selector_has_position
+    )
 
     # Do we keep the right atoms?
-    assert list(filtered.nodes()) == to_keep
+    assert list(filtered) == to_keep
     
-    # Do we actually have a copy?
-    assert filtered is not molecule
-    for atom in filtered.nodes.values():
-        atom['is_touched'] = True
-    assert not [atom for atom in molecule.nodes.values() if 'is_touched' in atom]
-
 
 def test_read_dssp2():
-    with open(DSSP_OUTPUT) as infile:
+    with open(str(DSSP_OUTPUT)) as infile:
         secondary_structure = dssp.read_dssp2(infile)
     assert secondary_structure == SECSTRUCT_1BTA
 
@@ -94,11 +92,11 @@ def test_run_dssp(savefile, tmpdir):
         path = tmpdir.join('dssp_output')
     else:
         path = None
-    system = m2.System()
-    system.add_molecule(read_pdb(PDB_PROTEIN))
+    system = vermouth.System()
+    system.add_molecule(read_pdb(str(PDB_PROTEIN)))
     secondary_structure = dssp.run_dssp(system,
                                         executable=DSSP_EXECUTABLE,
-                                        savefile=path)
+                                        savefile=str(path))
 
     # Make sure we produced the expected sequence of secondary structures
     assert secondary_structure == SECSTRUCT_1BTA
@@ -109,11 +107,11 @@ def test_run_dssp(savefile, tmpdir):
     # If we test without savefile, then we need to make sure the file is not
     # created.
     if savefile:
-        assert os.path.exists(path)
-        with open(path) as genfile, open(DSSP_OUTPUT) as reffile:
+        assert path.exists()
+        with open(str(path)) as genfile, open(str(DSSP_OUTPUT)) as reffile:
             gen = '\n'.join(genfile.readlines()[6:])
             ref = '\n'.join(reffile.readlines()[6:])
             assert gen == ref
     else:
         # Is the directory empty?
-        assert not os.listdir(tmpdir)
+        assert not os.listdir(str(tmpdir))
