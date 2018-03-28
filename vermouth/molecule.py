ee8a25205009a3ae7d96a689b2adcba7cf37c68f
Jonathan Barnoud <jonathan@barnoud.net>
2018-03-28 13:54:42 +0200

    Fix historic typo in dihedral effectors
    The `apply` method became `_apply` at some point.

--- vermouth/molecule.py
+++ vermouth/molecule.py
@@ -259,7 +259,7 @@ class ParamDihedral(LinkParameterEffector):
     """
     n_keys_asked = 4
 
-    def apply(self, molecule, keys):
+    def _apply(self, molecule, keys):
         # This will raise a ValueError if an atom is missing, or if an
         # atom does not have position.
         positions = np.stack([molecule.nodes[key]['position'] for key in keys])
@@ -276,7 +276,7 @@ class ParamDihedralPhase(LinkParameterEffector):
     """
     n_keys_asked = 4
 
-    def apply(self, molecule, keys):
+    def _apply(self, molecule, keys):
         # This will raise a ValueError if an atom is missing, or if an
         # atom does not have position.
         positions = np.stack([molecule.nodes[key]['position'] for key in keys])
