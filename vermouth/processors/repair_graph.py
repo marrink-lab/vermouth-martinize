2f9e19aa13b86f2d972489166fb00b2e3d4f480a
Peter Kroon <p.c.kroon@rug.nl>
2018-07-05 14:48:09 +0200

    Move Travis to Xenial, maybe that'll make py3.7 work
    Supposedly, adding sudo magic might help
    
    one more docstring to poke travis
    
    More Travis wrangling
    
    Travis config is surprisingly hard :(
    
    Really, really hard.
    
    And the documentation is not great either
    
    So close
    
    There we go :D

--- vermouth/processors/repair_graph.py
+++ vermouth/processors/repair_graph.py
@@ -13,7 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""
+Provides a processor that repairs a graph based on a reference.
+"""
 import networkx as nx
 
 from .processor import Processor
