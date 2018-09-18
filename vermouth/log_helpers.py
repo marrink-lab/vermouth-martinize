495ff760074cfae8266b829792090e1c43a77b48
Peter Kroon <p.c.kroon@rug.nl>
2018-09-18 15:51:56 +0200

    Improve loghelper logic, write tests

--- vermouth/log_helpers.py
+++ vermouth/log_helpers.py
@@ -18,7 +18,54 @@
 Provide some helper classes to allow new style brace formatting for logging and
 processing the `type` keyword.
 """
+import inspect
 import logging
+
+
+class PassingLoggerAdapter(logging.LoggerAdapter):
+    """
+    Helper class that figures out which keyword arguments are known by the log
+    method of this :class:`logging.LoggerAdapter`, and it's `logger` (which
+    might be another :class:`logging.LoggerAdapter`). Subclasses can use this
+    information to remove unknown keywords from their `super().log` calls.
+
+    Attributes
+    ----------
+    known_kwargs: set[str]
+        A union of all keywords known by this object's `log` method and
+        :attr:`child_kwargs`.
+    child_kwargs: set[str]
+        A set of all keywords known by this object's `logger`'s `log` method.
+    """
+    def __init__(self, logger, extra=None):
+        if extra is None:
+            extra = {}
+        super().__init__(logger, extra)
+
+    @property
+    def logger(self):
+        return self._logger
+
+    @logger.setter
+    def logger(self, new_val):
+        self._logger = new_val
+        self._find_kwargs()
+
+    def _find_kwargs(self):
+        logsig = inspect.signature(self.log)
+        my_kwargs = set(name for name, param in logsig.parameters.items()
+                        if param.kind not in [param.VAR_POSITIONAL, param.VAR_KEYWORD])
+        if isinstance(self._logger, logging.Logger):
+            child_kwargs = {'lvl', 'msg', 'exc_info', 'stack_info', 'extra'}
+        else:
+            if hasattr(self._logger, 'known_kwargs'):
+                child_kwargs = self._logger.known_kwargs
+            else:
+                parameters = inspect.signature(self._logger.log).parameters
+                child_kwargs = set(name for name, param in parameters.items()
+                                   if param.kind not in [param.VAR_POSITIONAL, param.VAR_KEYWORD])
+        self.known_kwargs = my_kwargs.union(child_kwargs)
+        self.child_kwargs = child_kwargs
 
 
 class Message:
@@ -26,38 +73,31 @@ class Message:
     Class that defers string formatting until it's ``__str__`` method is
     called.
     """
-    def __init__(self, fmt, args):
+    def __init__(self, fmt, args, kwargs):
         self.fmt = fmt
         self.args = args
+        self.kwargs = kwargs
 
     def __str__(self):
-        return self.fmt.format(*self.args)
+        return self.fmt.format(*self.args, **self.kwargs)
 
     def __repr__(self):
-        return '{}.format(*{})'.format(self.fmt, self.args)
+        return '"{}".format(*{}, **{})'.format(self.fmt, self.args, self.kwargs)
 
 
-class StyleAdapter(logging.LoggerAdapter):
+class StyleAdapter(PassingLoggerAdapter):
     """
     Logging adapter that encapsulate messages in :class:`Message`, allowing
     ``{}`` style formatting.
     """
-    def __init__(self, logger, extra=None):
-        if extra is None:
-            extra = {}
-        super().__init__(logger, extra)
-
-    def log(self, level, msg, *args, **kwargs):
-        if self.isEnabledFor(level):
-            msg, kwargs = self.process(msg, kwargs)
-            self.logger.log(level, Message(msg, args), **kwargs)
-
-    def __getattr__(self, item):
-        # Delegate attribute lookup further down the chain of LoggerAdapters
-        return getattr(self.logger, item)
+    def log(self, lvl, msg, *args, **kwargs):
+        msg, kwargs = self.process(msg, kwargs)
+        chain_kwargs = {key: val for key, val in kwargs.items()
+                        if key in self.child_kwargs}
+        self.logger.log(lvl, Message(msg, args, kwargs), **chain_kwargs)
 
 
-class TypeAdapter(logging.LoggerAdapter):
+class TypeAdapter(PassingLoggerAdapter):
     """
     Logging adapter that takes the `type` keyword argument passed to logging
     calls and passes adds it to the `extra` attribute.
@@ -72,22 +112,17 @@ class TypeAdapter(logging.LoggerAdapter):
         The type of the messages if none is given.
     """
     def __init__(self, logger, extra=None, default_type='general'):
-        if extra is None:
-            extra = {}
         self.default_type = default_type
         super().__init__(logger, extra)
 
-    def log(self, level, msg, *args, type=None, **kwargs):  # pylint: disable=arguments-differ
+    def log(self, *args, type=None, **kwargs):  # pylint: disable=arguments-differ
         if type is None:
             type = self.default_type
-        if self.isEnabledFor(level):
-            msg, kwargs = self.process(msg, kwargs)
-            kwargs['extra'].update(type=type)
-            self.logger.log(level, msg, *args, **kwargs)
-
-    def __getattr__(self, item):
-        # Delegate attribute lookup further down the chain of LoggerAdapters
-        return getattr(self.logger, item)
+        if 'extra' not in kwargs:
+            kwargs['extra'] = dict(type=type)
+        elif 'type' not in kwargs['extra']:
+            kwargs['extra']['type'] = type
+        self.logger.log(*args, **kwargs)
 
 
 def get_logger(name):
---
+++ vermouth/tests/test_logging.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 University of Groningen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Tests for the log helpers
+"""
+
+import logging
+import string
+
+from hypothesis import strategies as st
+from hypothesis import given, note, event, example
+import pytest
+
+from vermouth.log_helpers import (get_logger, TypeAdapter, StyleAdapter,
+                                  PassingLoggerAdapter)
+
+
+class LogHandler(logging.NullHandler):
+    def set_test(self, test):
+        self.test = test
+
+    def handle(self, record):
+        self.test(record)
+
+
+@pytest.fixture(scope='module')
+def logger():
+    logger_ = logging.getLogger(__name__)
+    logger_.setLevel(1)
+    handler = LogHandler(1)
+    logger_.addHandler(handler)
+    return logger_, handler
+
+
+@given(name=st.text())
+def test_get_logger(name):
+    default_logger = logging.getLogger(name)
+    vm_logger = get_logger(name)
+    assert vm_logger.logger is default_logger
+
+
+@example(args=[], type='general', default_type='general')
+@given(args=st.lists(st.text(), min_size=0, max_size=5),
+       type=st.one_of(st.none(), st.text()),
+       default_type=st.text(min_size=1))
+def test_type_adapter(logger, args, type, default_type):
+    def test(record):
+        rectype = getattr(record, 'type', None)
+        if type is None:
+            assert rectype == default_type
+        else:
+            assert rectype == type
+    logger, handler = logger
+    logger = TypeAdapter(logger, default_type=default_type)
+    handler.set_test(test)
+    fmt = ['%s']*len(args)
+    fmt = ' '.join(fmt)
+
+    note('fmt={}'.format(fmt))
+
+    if type is None:
+        logger.info(fmt, *args)
+        event('type is None')
+    else:
+        logger.info(fmt, *args, type=type)
+
+
+@example(args=[], kwargs={}, type='general', default_type='general')
+@given(args=st.lists(st.text(), min_size=0, max_size=5),
+       kwargs=st.dictionaries(st.text(alphabet=string.ascii_letters, min_size=1),
+                              st.text(), min_size=0, max_size=5),
+       type=st.one_of(st.none(), st.text()),
+       default_type=st.text(min_size=1))
+def test_style_type_adapter(logger, args, kwargs, type, default_type):
+    def test(record):
+        rectype = getattr(record, 'type', None)
+        print(rectype, type, default_type)
+        if type is None:
+            return rectype == default_type
+        else:
+            return rectype == type
+    logger, handler = logger
+    logger = TypeAdapter(logger, default_type=default_type)
+    logger = StyleAdapter(logger)
+    handler.set_test(test)
+    fmt = ['{}']*len(args) + ['{'+name+'}' for name in kwargs]
+    fmt = ' '.join(fmt)
+
+    note('fmt={}'.format(fmt))
+
+    if type is None:
+        logger.info(fmt, *args, **kwargs)
+        event('type is None')
+    else:   
+        logger.info(fmt, *args, **kwargs, type=type)
+
+
+@example(args=[], kwargs={})
+@given(args=st.lists(st.text(), min_size=0, max_size=5),
+       kwargs=st.dictionaries(st.text(alphabet=string.ascii_letters, min_size=1),
+                              st.text(), min_size=0, max_size=5),
+      )
+def test_style_adapter(logger, args, kwargs):
+    def test(record):
+        assert record.getMessage() == expected
+    logger, handler = logger
+    logger = StyleAdapter(logger)
+    handler.set_test(test)
+    fmt = ['{}']*len(args) + ['{'+name+'}' for name in kwargs]
+    fmt = ' '.join(fmt)
+
+    note('fmt={}'.format(fmt))
+    expected = fmt.format(*args, **kwargs)
+
+    logger.info(fmt, *args, **kwargs)
+
+
+@given(args=st.lists(st.text(), min_size=0, max_size=5),
+       kwargs=st.dictionaries(st.text(alphabet=string.ascii_letters, min_size=1),
+                              st.text(), min_size=1, max_size=5),
+      )
+def test_passing_adapter(logger, args, kwargs):
+    logger, handler = logger
+    handler.set_test(lambda: None)
+    logger = PassingLoggerAdapter(logger)
+    fmt = ['%s']*len(args) + ['%('+name+')s' for name in kwargs]
+    with pytest.raises(TypeError):
+        logger.info(fmt, *args, **kwargs)
