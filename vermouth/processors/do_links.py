91115646dc1622a782d036092f00f1888015dfce
NanduTej <tejananduri97@gmail.com>
2018-12-13 00:10:17 +0530

    Fixing an error in the previous change.

--- vermouth/processors/do_links.py
+++ vermouth/processors/do_links.py
@@ -283,7 +283,7 @@ class DoLinks(Processor):
             for match in matches:
                 for node, node_attrs in link.nodes.items():
                     if 'replace' in node_attrs:
-                        if node_attrs['replace'].get('atomname', False):
+                        if node_attrs['replace'].get('atomname', False) is None:
                             _nodes_to_remove.append(match[node])
                         else:
                             node_mol = molecule.nodes[match[node]]
