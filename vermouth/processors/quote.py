3fe85ba1e73ce31195ba1f4d8103109bb98dc26b
Peter Kroon <p.c.kroon@rug.nl>
2019-02-25 16:27:39 +0100

    Add docstrings

--- vermouth/processors/quote.py
+++ vermouth/processors/quote.py
@@ -31,6 +31,19 @@ QUOTE_FILE = os.path.join(DATA_PATH, 'quotes.txt')
 
 
 def read_quote_file(filehandle):
+    """
+    Iterates over `filehandle`, and yields all strings that are not empty.
+
+    Parameters
+    ----------
+    filehandle: collections.abc.Iterable[str]
+        A file opened for reading.
+
+    Yields
+    ------
+    str
+        All stripped elements of `filehandle` that are not empty.
+    """
     for line in filehandle:
         line = line.strip()
         if line:
@@ -38,12 +51,34 @@ def read_quote_file(filehandle):
 
 
 class Quoter(Processor):
+    """
+    Processor that can produce random string taken from a file. Useful for e.g.
+    quotes.
+
+    Parameters
+    ----------
+    quote_file: pathlib.Path or str
+        The path of the file containing the strings. Must contain at least one
+        line.
+    """
     def __init__(self, quote_file=None):
         if quote_file is None:
             quote_file = QUOTE_FILE
         self._quote_file = quote_file
 
     def run_system(self, system):
+        """
+        Logs a random line from the file passed at initialization.
+
+        Parameters
+        ----------
+        system
+            Not used
+
+        Returns
+        -------
+        None
+        """
         with open(self._quote_file) as handle:
             all_quotes = list(read_quote_file(handle))
         LOGGER.info(random.choice(all_quotes))
