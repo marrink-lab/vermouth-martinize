4c3772b2a1ede086b6b964e6c52ad16d4bd52025
Jonathan Barnoud <jonathan@barnoud.net>
2019-02-26 11:29:32 +0100

    Upgrade the GoMartini processor to add the virtual sites in the structure and the topology

--- vermouth/tests/test_go_vs_includes.py
+++ vermouth/tests/test_go_vs_includes.py
@@ -20,33 +20,58 @@ import pytest
 from vermouth import Molecule
 from vermouth.processors import GoVirtIncludes
 
-def test_go_virt_includes():
+@pytest.fixture(params=(0, 4))
+def molecule_for_go(request):
+    """
+    Molecule to test virtual site-based GoMartini.
+
+    The molecule has a variable number of residue set in the fixture parameter.
+    Each residue has a backbone and a side chain beads. It also has some post
+    sections lines that should not disapear when running the processor, and a
+    moltype name as this is required.
+    """
+    n_residues = request.param
+    molecule = Molecule()
+    for residue_idx in range(n_residues):
+        base_atom_idx = residue_idx * 2
+        for relative_atom_idx, atomname in enumerate(('BB', 'SC1')):
+            molecule.add_node(
+                base_atom_idx + relative_atom_idx,
+                resid=residue_idx,
+                resname='XX',
+                atomname=atomname,
+                charge_group=base_atom_idx,
+                chain='A',
+                # Should be an array, but it is not relevant for the test
+                position=[0, 0, 0],  
+            )
+    molecule.meta['post_section_lines'] = {
+        'exclusions': ['I exist'],
+        'other': ['Hello'],
+    }
+    molecule.meta['moltype'] = 'TEST'
+    return molecule
+
+
+def test_go_virt_includes(molecule_for_go):
     """
     Test the processor in normal conditions.
     """
-    molecule = Molecule()
-    molecule.meta['pre_section_lines'] = {
-        'virtual_sitesn': ['I was there before'],
-    }
-    molecule.meta['post_section_lines'] = {
-        'atoms': ['I exist'],
-    }
-    molecule.meta['moltype'] = 'TEST'
-
+    molecule = molecule_for_go
+    initial_n_backbone = len(molecule.nodes) // 2  # Each residues has 2 atoms
     processor = GoVirtIncludes()
     processor.run_molecule(molecule)
 
-    expected_pre = {
-        'virtual_sitesn': ['I was there before'],
-    }
     expected_post = {
-        'atoms': ['I exist', '#include "TEST_atoms_VirtGoSite.itp"'],
-        'virtual_sitesn': ['#include "TEST_virtual_sitesn_VirtGoSite.itp"'],
-        'exclusions': ['#include "TEST_exclusions_VirtGoSite.itp"'],
+        'exclusions': ['I exist', '#include "TEST_exclusions_VirtGoSite.itp"'],
+        'other': ['Hello'],
     }
 
-    assert molecule.meta['pre_section_lines'] == expected_pre
     assert molecule.meta['post_section_lines'] == expected_post
+    # Each residue in the initial molecule has a backbone and a side chain
+    # beads. We create one virtual site per backbone beads, so we expect to
+    # have a side chain and a virtual site for each backbone bead.
+    assert len(molecule.nodes) == initial_n_backbone * 3
 
 
 def test_go_virt_includes_no_moltype():
