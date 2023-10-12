01b617b0a307acd3e36d37dc788c46b211e00f48
Fabian Gruenewald <f.grunewald@rug.nl>
2023-10-12 16:36:05 +0200

    implement go_pipeline

--- vermouth/rcsu/go_pipeline.py
+++ vermouth/rcsu/go_pipeline.py
@@ -18,19 +18,39 @@ from vermouth.processors import Processor
 from .go_vs_includes import GoVirtIncludes
 from .go_structure_bias import ComputeStructuralGoBias
 from .go_water_bias import ComputeWaterGoBias
+from ..processors import SetMoleculeMeta
 
-class ProcessorPipline(Processor):
+class GoProcessorPipline(Processor):
     """
     Wrapping all processors for the go model.
     """
     def __init__(self, processor_list, **kwargs):
         self.processor_list = processor_list
 
+    def prepare_run(self, system):
+        """
+        Things to do before running the pipeline.
+        """
+        # merge all molecules in the system
+        # this will eventually become deprecated
+        # with the proper Go-model for multimers
+        vermouth.MergeAllMolecules().run_system(system)
+        for molecule in system.molecules:
+            res_graph = vermouth.graph_utils.make_residue_graph(system.molecules[0])
+            molecule.residue_graph = res_graph
+
+    def postprocess_run(self, system):
+        """
+        Do required post-processing.
+        """
+        pass
+
     def run_system(self, system):
+        self.prepare_run(self, system)
         for processor in processor_list:
             process_args = inspect.getfullargspec(processor).args
             process_args_values = {self.kwargs[arg] for arg in process_args}
             processor(**process_args_values).run_system(system)
     return system
 
-GoPipeline = ProcessorPipline([GoVirtIncludes, ComputeStructuralGoBias, ComputeWaterGoBias])
+GoPipeline = ProcessorPipline([SetMoleculeMeta, GoVirtIncludes, ComputeStructuralGoBias, ComputeWaterGoBias])
