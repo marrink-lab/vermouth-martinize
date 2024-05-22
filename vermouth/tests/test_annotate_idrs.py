cd0d5bd299903d8163c43687a3c0e018efec7480
csbrasnett <c.s.brasnett@rug.nl>
2024-05-22 14:46:19 +0200

    changed test_annotate_idrs.py to test system not molecule

--- vermouth/tests/test_annotate_idrs.py
+++ vermouth/tests/test_annotate_idrs.py
@@ -17,45 +17,44 @@ Test for the tune idp bonds processor.
 """
 import pytest
 from vermouth.processors.annotate_idrs import AnnotateIDRs
-from .datafiles import PDB_ALA5_CG
-from vermouth.pdb.pdb import read_pdb
-import networkx as nx
-
-def protein():
-    """
-    Read a PDB file describing a protein at CG resolution.
-    Embed the content of the PDB in a :class:`vermouth.system.System`.
-    """
-    molecules = read_pdb(PDB_ALA5_CG)
-    assert len(molecules) == 1
-    molecule = molecules[0]
-    molecule.remove_edges_from(list(molecule.edges))
-    _resids = nx.get_node_attributes(molecule, "resid")
-    nx.set_node_attributes(molecule, _resids, "_old_resid")
-    return molecule
+from vermouth.tests.helper_functions import create_sys_all_attrs, test_molecule
 
 @pytest.mark.parametrize('idr_regions, expected', [
     (
-    [(1,3)],
-    [True, True, True, True, True, True, False, False, False, False]
-    ),
-    (
-    [(1,2),(4,5)],
-    [True, True, True, True, False, False, True, True, True, True]
+    [(1,2)],
+    [True, True, True, True, True, False, False, False, False]
     ),
     (
     [],
-    [False, False, False, False, False, False, False, False, False, False]
+    [False, False, False, False, False, False, False, False, False]
     )
 ])
-def test_make_disorder_string(idr_regions, expected):
-    molecule = protein()
-    AnnotateIDRs(id_regions=idr_regions).run_molecule(molecule)
+def test_make_disorder_string(test_molecule,
+                              idr_regions,
+                              expected):
+    atypes = {0: "P1", 1: "SN4a", 2: "SN4a",
+              3: "SP1", 4: "C1",
+              5: "TP1",
+              6: "P1", 7: "SN3a", 8: "SP4"}
+    # the molecule resnames
+    resnames = {0: "A", 1: "A", 2: "A",
+                3: "B", 4: "B",
+                5: "C",
+                6: "D", 7: "D", 8: "D"}
+    secstruc = {1: "H", 2: "H", 3: "H", 4: "H"}
+
+    system = create_sys_all_attrs(test_molecule,
+                                  moltype="molecule_0",
+                                  secstruc=secstruc,
+                                  defaults={"chain": "A"},
+                                  attrs={"resname": resnames,
+                                         "atype": atypes})
+
+    AnnotateIDRs(id_regions=idr_regions).run_system(system)
     result = []
-    for key, node in molecule.nodes.items():
-        if molecule.nodes[key].get("cgidr"):
+    for key, node in system.molecules[0].nodes.items():
+        if system.molecules[0].nodes[key].get("cgidr"):
             result.append(True)
         else:
             result.append(False)
     assert result == expected
-
