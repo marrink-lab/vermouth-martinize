ba02c224e8ec52e0566b9e92881e19031b84586d
Chris Brasnett <35073246+csbrasnett@users.noreply.github.com>
2024-12-07 17:33:17 +0100

    change to using _res_serial attribute, so we can deal with > 1 chain

--- vermouth/rcsu/contact_map.py
+++ vermouth/rcsu/contact_map.py
@@ -305,13 +305,13 @@ def res2atom(arrin, residues, nresidues):
     take an array with residue level data and repeat the entries over
     each atom within the residue
 
-    would be nice to do this with list comprension but Ican't work out how
+    would be nice to do this with list comprehension but I can't work out how
     to do something like:
 
-    [np.tile(res_dists[i-1,j-1],
+    [np.tile(res_dists[i,j],
              np.where(residues == i)[0].size)
-        for i in range(1, nresidues+1)
-        for j in range(1, nresidues+1)
+        for i in range(nresidues)
+        for j in range(nresidues)
     ]
 
     to get it correct in the 2 dimensional way we actually need.
@@ -329,9 +329,9 @@ def res2atom(arrin, residues, nresidues):
 
     out = np.zeros((len(residues), len(residues)))
     start0 = 0
-    for i, j in zip(unique_values - 1, counts):
+    for i, j in zip(unique_values, counts):
         start1 = 0
-        for k, l in zip(unique_values - 1, counts):
+        for k, l in zip(unique_values, counts):
             target_value = arrin[i, k]
             out[start0:start0 + j, start1:start1 + l] = target_value.sum()
             start1 += l
@@ -345,8 +345,8 @@ def atom2res(arrin, residues, nresidues, norm=False):
     take an array with atom level data and sum the entries over within the residue
     '''
     out = np.array([int(arrin[np.where(residues == i)[0], np.where(residues == j)[0][:, np.newaxis]].sum())
-                    for i in range(1, nresidues + 1)
-                    for j in range(1, nresidues + 1)]).reshape((nresidues, nresidues))
+                    for i in range(nresidues)
+                    for j in range(nresidues)]).reshape((nresidues, nresidues))
     if norm:
         out[out > 0] = 1
 
@@ -402,10 +402,12 @@ def calculate_contact_map(mol):
     resnames = []
     ca_pos = []
     chains = []
+    res_serial = []
     for node in mol.nodes:
         try:
             coords.append(mol.nodes[node]['position'] * 10)  #  need *10 here because of the way Vermouth read coords
             residues.append(mol.nodes[node]['resid'])
+            res_serial.append(mol.nodes[node]['_res_serial'])
             resnames.append(mol.nodes[node]['resname'])
             vdw_list.append(get_vdw_radius(mol.nodes[node]['resname'],
                                            mol.nodes[node]['atomname']))
@@ -421,9 +423,10 @@ def calculate_contact_map(mol):
     residues = np.array(residues)
     vdw_list = np.array(vdw_list)
     atypes = np.array(atypes)
+    res_serial = np.array(res_serial)
 
     # 2) find the number of residues that we have
-    nresidues = len(list(set(residues)))
+    nresidues = len(list(set(res_serial)))
 
     # 4) set up final bits of information
     # sums of vdw pairs for each atom we have
@@ -436,23 +439,26 @@ def calculate_contact_map(mol):
     diagonal_ones = np.diagflat(np.ones(atomic_distances.shape[0], dtype=int))
 
     # get the coordinates of the centres of geometry for each residue
-    res_cogs = np.stack(
-        [np.stack(coords[np.where(residues == i)[0][0]:np.where(residues == i)[0][-1]]).mean(axis=0) for i in
-         range(1, nresidues + 1)])
-    res_dists = res2atom(cdist(res_cogs, res_cogs), residues, nresidues)
+    res_cogs = np.stack([np.stack(coords[np.where(res_serial == i)[0][0]:
+                                         np.where(res_serial == i)[0][-1]]).mean(axis=0)
+                         for i in range(nresidues)])
+    res_dists = res2atom(cdist(res_cogs, res_cogs), res_serial, nresidues)
 
     # 5) find atoms which meet the overlap criterion
     over = np.zeros_like(atomic_distances)
-    overlaps = np.where((atomic_distances <= (vdw_sum_array * alpha)) & (diagonal_ones != 1) & (res_dists < 14))
+    overlaps = np.where((atomic_distances <= (vdw_sum_array * alpha)) &
+                        (diagonal_ones != 1) &
+                        (res_dists < 14))
     over[overlaps[0], overlaps[1]] = 1
 
     # 6) set up the surface overlap criterion
     # generate fibonacci spheres for all atoms.
     # can't decide whether quicker/better for memory to generate all in one go here
-    # or incorporate into the loop. left
+    # or incorporate into the loop. code to do it more on the fly is left in the loop for now
     spheres = np.stack([make_surface(i, fiba, fibb, water_radius + j) for i, j in zip(coords, vdw_list)])
-    surface_overlaps = np.where(
-        (atomic_distances <= (vdw_sum_array + water_radius)) & (diagonal_ones != 1) & (res_dists < 14))
+    surface_overlaps = np.where((atomic_distances <= (vdw_sum_array + water_radius)) &
+                                (diagonal_ones != 1) &
+                                (res_dists < 14))
     # find which atoms are uniquely involved as base points
     base_points = np.unique(surface_overlaps[0])
 
@@ -509,13 +515,14 @@ def calculate_contact_map(mol):
                     if btype == 5:
                         destabilisercounter_1[i, k] += 1
 
-    overlapcounter_2 = atom2res(over, residues, nresidues, norm=True)
-    contactcounter_2 = atom2res(contactcounter_1, residues, nresidues)
-    stabilisercounter_2 = atom2res(stabilisercounter_1, residues, nresidues)
-    destabilisercounter_2 = atom2res(destabilisercounter_1, residues, nresidues)
+    overlapcounter_2 = atom2res(over, res_serial, nresidues, norm=True)
+    contactcounter_2 = atom2res(contactcounter_1, res_serial, nresidues)
+    stabilisercounter_2 = atom2res(stabilisercounter_1, res_serial, nresidues)
+    destabilisercounter_2 = atom2res(destabilisercounter_1, res_serial, nresidues)
 
-    resnames = np.array(resnames)[np.unique(residues, return_index=True)[1]]
-    resids = np.array(residues)[np.unique(residues, return_index=True)[1]]
+    # resnames = np.array(resnames)[np.unique(res_serial, return_index=True)[1]]
+    resids = np.array(residues)[np.unique(res_serial, return_index=True)[1]]
+    chains = np.array(chains)[np.unique(res_serial, return_index=True)[1]]
 
     # this to write out the file if needed
     # with open('contact_map_out.out', 'w') as f:
@@ -532,8 +539,8 @@ def calculate_contact_map(mol):
     #             if (over > 0 or cont > 0) and (i1 != i2):
     #                 count += 1
     #                 msg = (f"R {int(count):6d} "
-    #                        f"{int(i1 + 1):5d}  {resnames[i1]:3s} A {int(resids[i1]):4d}    "
-    #                        f"{int(i2 + 1):5d}  {resnames[i2]:3s} A {int(resids[i2]):4d}    "
+    #                        f"{int(i1 + 1):5d}  {resnames[i1]:3s} {chains[i1]:1s} {int(resids[i1]):4d}    "
+    #                        f"{int(i2 + 1):5d}  {resnames[i2]:3s} {chains[i2]:1s} {int(resids[i2]):4d}    "
     #                        f"{euclidean(ca_pos[i1], ca_pos[i2]) * 10:9.4f}     "
     #                        f"{int(over):1d} {1 if cont != 0 else 0} {1 if ocsu != 0 else 0} {1 if rcsu > 0 else 0}"
     #                        f"{int(rcsu):6d}  {int(cont):6d}\n")
@@ -552,9 +559,7 @@ def calculate_contact_map(mol):
             if (over > 0 or cont > 0) and (i1 != i2):
                 if over == 1 or (over == 0 and rcsu == 1):
                     # this is a OV or rCSU contact we take it
-                    chain_i1 = chains[np.where(residues == i1+1)[0][0]]
-                    chain_i2 = chains[np.where(residues == i2+1)[0][0]]
-                    contacts.append((i1+1, chain_i1, i2+1, chain_i2))
+                    contacts.append((resids[i1], chains[i1], resids[i2], chains[i2]))
     return contacts
 
 
