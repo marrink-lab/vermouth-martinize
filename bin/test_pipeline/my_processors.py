69e4601f010d4c1a75f4abaa552c9ca94b538b03
HenkBroam <Roorda2001@gmail.com>
2026-04-13 11:19:47 +0200

    Added the elastic network with all the parameters and functions of martinize2

--- bin/test_pipeline/my_processors.py
+++ bin/test_pipeline/my_processors.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from vermouth import DATA_PATH
 from vermouth.map_input import read_mapping_directory
 import vermouth
+import functools
 from vermouth import selectors
 from vermouth.rcsu.contact_map import read_go_map, GenerateContactMap
 from vermouth.rcsu.go_pipeline import GoPipeline
@@ -335,3 +336,79 @@ class MergeChainsWrapper(Processor):
                                 "Either specify -merge all or -merge A,B,C (+).")
             return system   
         
+class ElasticWrapper(Processor):
+    def __init__(
+        self, 
+        rb_force_constant,
+        rb_lower_bound,
+        rb_upper_bound,
+        rb_decay_factor,
+        rb_decay_power,
+        rb_minimum_force,
+        rb_selection,
+        rb_unit,
+        res_min_dist,
+    ):
+       self.rb_force_constant = rb_force_constant
+       self.rb_lower_bound = rb_lower_bound
+       self.rb_upper_bound = rb_upper_bound
+       self.rb_decay_factor = rb_decay_factor
+       self.rb_decay_power = rb_decay_power
+       self.rb_minimum_force = rb_minimum_force
+       self.rb_selection = rb_selection
+       self.rb_unit = rb_unit
+       self.res_min_dist = res_min_dist
+    def run_system(self, system):
+        LOGGER.info("Setting the rubber bands.", type="step")
+        if self.rb_unit == "molecule":
+            domain_criterion = vermouth.processors.apply_rubber_band.always_true
+        elif self.rb_unit == "all":
+            vermouth.MergeAllMolecules().run_system(system)
+            domain_criterion = vermouth.processors.apply_rubber_band.always_true
+        elif self.rb_unit == "chain":
+            domain_criterion = vermouth.processors.apply_rubber_band.same_chain
+        else:
+            regions = [
+                tuple(int(i) for i in apair.split(":"))
+                for apair in self.rb_unit.split(",")
+            ]
+            if any(len(region) != 2 for region in regions):
+                message = (
+                    'Faulty resid interval for elastic network unit: "{}".'.format(
+                        self.rb_unit
+                    )
+                )
+                LOGGER.critical(message)
+                raise ValueError(message)
+            else:
+                domain_criterion = (
+                    vermouth.processors.apply_rubber_band.make_same_region_criterion(
+                        regions
+                    )
+                )
+
+        if self.rb_selection is not None:
+            selector = functools.partial(
+                selectors.proto_select_attribute_in,
+                attribute="atomname",
+                values=self.rb_selection,
+            )
+        else:
+            selector = functools.partial(selectors.select_backbone,
+                                         bb_atomname=system.force_field.variables['bb_atomname'])
+        rubber_band_processor = vermouth.ApplyRubberBand(
+            lower_bound=self.rb_lower_bound,
+            upper_bound=self.rb_upper_bound,
+            decay_factor=self.rb_decay_factor,
+            decay_power=self.rb_decay_power,
+            base_constant=self.rb_force_constant,
+            minimum_force=self.rb_minimum_force,
+            selector=selector,
+            domain_criterion=domain_criterion,
+            res_min_dist=self.res_min_dist,
+        )
+        rubber_band_processor.run_system(system)
+        return system
+        
+
+       
